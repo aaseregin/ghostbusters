bafa4a63c46a3e8b12d57d95abc9110314712bea
Andrey Seregin <a.a.seregin@yandex.ru>
2020-06-19 20:19:28 +0300

    сайт готов

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
   const mMenuBtn = $(".m-menu-button");
   const mMenu = $(".m-menu");
+  const tab = $(".tab");
   mMenuBtn.on("click", function () {
     mMenu.toggleClass("active");
     $("body").toggleClass("no-scroll");
   });
+
+  tab.on("click", function () {
+    tab.removeClass("active");
+    $(this).toggleClass("active");
+    let activeTabContetnt = $(this).attr("data-target");
+    $(".tabs-content").removeClass("visible");
+    $(activeTabContetnt).toggleClass("visible");
+  });
+
   var mySwiper = new Swiper ('.swiper-container', {
     slidesPerView: 4,
     spaceBetween: 25,
