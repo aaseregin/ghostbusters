5101868b591dc6c6574837f1847ac9d6975c4344
Andrey Seregin <a.a.seregin@yandex.ru>
2020-06-19 16:59:02 +0300

    начаты скрипты и ошибка при уменьшении размера

--- js/main.js
+++ js/main.js
@@ -5,4 +5,24 @@ $(document).ready(function() {
     mMenu.toggleClass("active");
     $("body").toggleClass("no-scroll");
   });
+  var mySwiper = new Swiper ('.swiper-container', {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    loop: true,
+    breakpoints: {
+      992: {
+        slidesPerView: 4,
+      },
+
+      768: {
+        slidesPerView: 2,
+      },
+      320: {
+        slidesPerView: 1,
+      },
+    },
+
+  });
+
 });
+
